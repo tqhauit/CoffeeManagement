7065ee306864c836d51edccc087bd50644fc0a7f
tmducit <tmducit@DESKTOP-K73O8J2>
2018-11-01 11:41:39 +0700

    hau push lan N

---
+++ CoffeeManagement/CoffeeManagement/Models/UserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeManagement.Models
+{
+    public class UserViewModel
+    {
+        public List<Employee> listEmployee;
+
+        public Employee employee;
+    }
+}
